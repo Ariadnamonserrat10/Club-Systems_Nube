e22f28a104848c3d6932ce0e1322651170296c35
RoldanUriel <uriroldan06@gmail.com>
2026-03-17 23:29:26 -0600

    Use same-origin backend when served from Heroku

--- src/services/backend.js
+++ src/services/backend.js
@@ -1,4 +1,7 @@
-const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://clubsystem-backend-ghexckbjh4dxhgff.canadacentral-01.azurewebsites.net";
+const envBackendUrl = import.meta.env.VITE_BACKEND_URL;
+const runtimeOrigin = typeof window !== "undefined" ? window.location.origin : "";
+const defaultBackendUrl = runtimeOrigin || "https://clubsystem-backend-ghexckbjh4dxhgff.canadacentral-01.azurewebsites.net";
+const backendUrl = envBackendUrl || defaultBackendUrl;
 
 // Avoid double slashes when building endpoint URLs in api.js
 export const BACKEND = backendUrl.replace(/\/+$/, "");
