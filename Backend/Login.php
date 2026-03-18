b7fecbc256ad8aaa24ebdf3b34ff31e010810c08
RoldanUriel <uriroldan06@gmail.com>
2026-03-17 23:27:31 -0600

    Serve frontend from Heroku backend root

--- Backend/Login.php
+++ Backend/Login.php
@@ -1,31 +1,10 @@
 <?php
-header("Access-Control-Allow-Origin: https://clubsystems.netlify.app");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-
-// tu lógica abajo...
-$data = json_decode(file_get_contents("php://input"), true);
-?>
-
-<?php
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+require_once __DIR__ . "/cors.php";
 header("Content-Type: application/json; charset=utf-8");
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-
 include __DIR__ . "/db.php";
-$data = json_decode(file_get_contents("php://input"), true);
+$rawBody = file_get_contents("php://input");
+$data = json_decode($rawBody, true);
 
 if (!$data) {
   http_response_code(400);
